e744536a074c5b8dbfa86be26d352be45fcb0644
damiancodes <damianbwire88@gmail.com>
2025-04-16 10:57:32 +0300

    commit

---
+++ app/src/main/java/com/firebaseone/data/AuthViewModel.kt
@@ -0,0 +1,159 @@
+package com.firebaseone.data
+import android.R.attr.progress
+import android.app.ProgressDialog
+import android.content.Context
+import android.widget.Toast
+import androidx.navigation.NavHostController
+import androidx.navigation.navOptions
+import com.firebaseone.model.User
+import com.firebaseone.naavigation.ROUTE_HOME
+import com.firebaseone.naavigation.ROUTE_LOGIN
+import com.firebaseone.naavigation.ROUTE_REGISTER
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
+//
+//class AuthViewModel(var navHosController: NavHostController, var context:Context) {
+//
+//    var mAuth: FirebaseAuth
+//
+//    init {
+//        mAuth = FirebaseAuth.getInstance()
+//    }
+//
+//    fun signup(email: String, pass: String, confpass: String) {
+//        if (email.isBlank() || pass.isBlank() || confpass.isBlank()) {
+//            Toast.makeText(
+//                context,
+//                "Please email and password cannot be  " + "blank",
+//                Toast.LENGTH_LONG
+//            ).show()
+//            return
+//        } else if (pass != confpass) {
+//            Toast.makeText(context, "password do not match", Toast.LENGTH_LONG).show()
+//            return
+//        } else {
+//            mAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
+//                if (it.isSuccessful) {
+//                    val userdata = User(email, pass.mAuth.currentUser!!.uid)
+//                    val regRef = FirebaseDatabase.getInstance().getReference()
+//                        .child("User/" + mAuth.currentUser!!.uid)
+//                    regRef.setValue(userdata).addOnCompleteListener {
+//
+//                        if (it.isSuccessful) {
+//                            Toast.makeText(context, "Registerded Successfuly", Toast.LENGTH_LONG)
+//                                .show()
+//                            navController.navigate(ROUTE_LOGIN)
+//                        }
+//                    }
+//
+//                } else {
+//                    navController.navigate(ROUTE_REGISTER)
+//                }
+//            }
+//        }
+//    }
+//
+//}
+//fun login(email: String,pass:String){
+//    progress.show()
+//
+//    mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListner {
+//        progress.dismiss()
+//
+//        if (it.isSuccesful) {
+//            Toast.makeText(context, "Successfully logged in", Toast.LENGTH_LONG).show()
+//            navController.navigate(ROUTE_HOME)
+//            navController.navigate(ROUTE_REGISTER) TO TAKE YOU TO A DIFFERENT PAGE
+//
+//        } else {
+//            Toast.makeText(context, "${it.exception!!.message}", Toast.LENGTH_LONG).show()
+//            navController.navigate(ROUTE_LOGIN)
+//
+//        }
+//    }
+//}
+//fun logout() {
+//    mAuth.signOut()
+//    navController.navigate(ROUTE_LOGIN)
+//}
+//fun isloggedin(): Boolean {
+//    return mAuth.currentUser != null
+//}
+//
+//    }
+
+
+
+
+
+
+class AuthViewModel(var navController:NavHostController,var context:Context){
+
+    var mAuth: FirebaseAuth
+    val progress: ProgressDialog
+
+    init {
+        mAuth= FirebaseAuth.getInstance()
+        progress=ProgressDialog(context)
+        progress.setTitle("Loading")
+        progress.setMessage("PLease Wait.....")
+    }
+    fun signup(email:String,pass:String,confpass:String){
+        progress.show()
+
+        if (email.isBlank() || pass.isBlank() ||confpass.isBlank()){
+            progress.dismiss()
+            Toast.makeText(context,"Please email and password cannot be blank",Toast.LENGTH_LONG).show()
+            return
+        }else if (pass != confpass){
+            Toast.makeText(context,"Password do not match",Toast.LENGTH_LONG).show()
+            return
+        }else{
+            mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener {
+                if (it.isSuccessful){
+                    val userdata= User(email,pass,mAuth.currentUser!!.uid)
+                    val regRef= FirebaseDatabase.getInstance().getReference()
+                        .child("Users/"+mAuth.currentUser!!.uid)
+                    regRef.setValue(userdata).addOnCompleteListener {
+
+                        if (it.isSuccessful){
+                            Toast.makeText(context,"Registered Successfully",Toast.LENGTH_LONG).show()
+                            navController.navigate(ROUTE_LOGIN)
+
+                        }else{
+                            Toast.makeText(context,"${it.exception!!.message}",Toast.LENGTH_LONG).show()
+                            navController.navigate(ROUTE_LOGIN)
+                        }
+                    }
+                }else{
+                    navController.navigate(ROUTE_REGISTER)
+                }
+
+            } }
+
+    }
+    fun login(email: String,pass: String){
+        progress.show()
+
+        mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener {
+            progress.dismiss()
+            if (it.isSuccessful){
+                Toast.makeText(context,"Succeffully Logged in",Toast.LENGTH_LONG).show()
+                navController.navigate(ROUTE_HOME)
+//                navController.navigate(ROUTE_REGISTER)TO TAKE YOU TO A DIIFFERNT PAGE
+            }else{
+                Toast.makeText(context,"${it.exception!!.message}",Toast.LENGTH_LONG).show()
+                navController.navigate(ROUTE_LOGIN)
+            }
+        }
+
+    }
+    fun logout(){
+        mAuth.signOut()
+        navController.navigate(ROUTE_LOGIN)
+    }
+    fun isloggedin():Boolean{
+        return mAuth.currentUser !=null
+    }
+
+}
